b7f411ffdbd2e69939d25ec428dadd3f100e8ce2
Rafael Batista <rafaabatistas@gmail.com>
2022-07-11 12:03:31 -0300

    chore: adicionando o NativeBaseProvider no App.tsx

--- App.tsx
+++ App.tsx
@@ -1,21 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, Box } from 'native-base';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar backgroundColor="transparent" style="light" />
-    </View>
+    <NativeBaseProvider>
+      <Box>Hello world</Box>
+      <StatusBar backgroundColor="transparent" style="dark" />
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});

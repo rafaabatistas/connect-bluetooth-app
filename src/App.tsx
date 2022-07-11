8d40eb8114bf567034bcdde5ed1407f67ea75c1b
Rafael Batista <rafaabatistas@gmail.com>
2022-07-11 12:58:02 -0300

    feat: trazendo a <Home /> para o App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { NativeBaseProvider, Box } from 'native-base';
+import { NativeBaseProvider } from 'native-base';
+
+import { Home } from './screens/Home/Home';
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Box>Hello world</Box>
       <StatusBar backgroundColor="transparent" style="dark" />
+      <Home />
     </NativeBaseProvider>
   );
 }

b64e9835b64d25be808b50a9c310809510d288af
Rafael Batista <rafaabatistas@gmail.com>
2022-07-12 13:30:11 -0300

    chore: adicionando o pathMapping no projeto

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,26 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: '.',
+          alias: {
+            '@src': './src',
+            '@components': './src/components',
+            '@atoms': './src/components/ui/atoms',
+            '@molecules': './src/components/ui/molecules',
+            '@organisms': './src/components/ui/organisms',
+            '@screens': './src/screens',
+            '@styles': './src/styles',
+            '@utils': './src/utils',
+            '@services': './src/services',
+            '@assets': './assets'
+          }
+        }
+      ]
+    ]
   };
 };

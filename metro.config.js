4ba1489b00f90080b5fefc973f9faba9cb04859e
Rafael Batista <rafaabatistas@gmail.com>
2022-07-14 11:54:07 -0300

    chore: adicionando configuração para o expo-updates

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
+
+config.transformer.assetPlugins = ['expo-asset/tools/hashAssetFiles'];
+
+module.exports = config;
